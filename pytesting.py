6e9e1ea17a472daa718fbcdf957567eb7e3ec886
Suhas <108694711+HiddenMachine3@users.noreply.github.com>
2023-11-18 15:24:37 +0530

    first commit

---
+++ pytesting.py
@@ -0,0 +1,10 @@
+def ha_func(kasdf: int, something: int) -> int:
+    """
+    This function does something.
+    """
+    return kasdf + something
+
+print(ha_func(1, 2))
+
+def test_something():
+    assert ha_func(1,2) == 3
